93a00f647776312c789c6416b633eb4d012c2fb4
sarthak <sarthak.ahuja0007@gmail.com>
2021-06-13 15:27:42 +0530

    Comment post

--- confusion/src/redux/commentsReducer.js
+++ confusion/src/redux/commentsReducer.js
@@ -1,14 +1,16 @@
 import * as ActionTypes from './actionTypes'
+import axios from 'axios'
+import baseUrl from '../shared/baseUrl'
 
-export const CommentsReducer=(state={
-    isLoading:true,
-    err:null,
-    comments:[]
-}, action)=>{
+export const CommentsReducer = (state = {
+	isLoading: true,
+	err: null,
+	comments: []
+}, action) => {
 
-    switch(action.type){
+	switch (action.type) {
 
-        case ActionTypes.Comments_Loading:
+		case ActionTypes.Comments_Loading:
 			{
 				return {
 					isLoading: true,
@@ -35,16 +37,18 @@ export const CommentsReducer=(state={
 				}
 			}
 
-        case ActionTypes.Add_Comment :
-            {
-                var comment=action.payload;
-                comment.id=state.length;
-                comment.date=new Date().toISOString();
-				console.log("============================");
-				return { ...state, comments: state.comments.concat(comment)};				
-            }
+		case ActionTypes.Add_Comment:
 
-        default:
-            return state;
-    }
+			{
+				var comment = action.payload;
+				comment.date = new Date().toISOString();
+				axios.post(baseUrl + 'comments', { ...comment })
+					.then(res=>{console.log(res)})
+					.catch(error => {console.log(error)})
+				return { ...state, comments: state.comments.concat(comment)};
+			}
+
+		default:
+			return state;
+	}
 }
